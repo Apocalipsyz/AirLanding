cc6aec5ae07802d7db2446381438feb40e85ac73
Apocalipsyz <apocalipsyz@mail.ru>
2015-03-15 23:02:27 +0300

    Final changes on project structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,3 +1,54 @@
-/**
- * Created by Medsestrun on 15.03.2015.
- */
+'use strict';
+
+var gulp = require('gulp'),
+    useref = require('gulp-useref'),
+    gulpif = require('gulp-if'),
+    uglify = require('gulp-uglify'),
+    prefix = require('gulp-autoprefixer'),
+    minifyCss = require('gulp-minify-css'),
+    connect = require('gulp-connect'),
+    wiredep = require('wiredep').stream;
+
+gulp.task('build', ['bower'], function () {
+    var assets = useref.assets();
+
+    return gulp.src('app/*.html')
+        .pipe(assets)
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', prefix('last 2 versions', '> 1%', 'ie 9')))
+        .pipe(minifyCss())
+        .pipe(assets.restore())
+        .pipe(useref())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+    gulp.src('./app/index.html')
+        .pipe(connect.reload());
+});
+
+gulp.task('css', function() {
+    gulp.src('./app/styles/*.css')
+        .pipe(connect.reload());
+});
+
+gulp.task('bower', function () {
+    gulp.src('./app/index.html')
+        .pipe(wiredep({
+            directory: "app/bower_components"
+        }))
+        .pipe(gulp.dest('./app'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch('bower.json', ['bower']);
+    gulp.watch('app/index.html', ['html']);
+    gulp.watch('app/styles/*.css', ['css']);
+});
+
+gulp.task('connect', ['watch'], function () {
+    connect.server({
+        root: 'app',
+        livereload: true
+    });
+});
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,1 @@
-/**
- * Created by Medsestrun on 13.03.2015.
- */
+
