465236b7ac8610459610d2d69cc6d3d4d6bb2ca2
Medsestrun <apocalipsyz@mail.ru>
2015-04-03 16:26:21 +0300

    Services section

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,16 @@
+$(function() {
 
+    var slideHeight = $(window).height();
+    $('#header-carousel .carousel-inner .item').css('height',slideHeight);
+
+    $(window).resize(function(){'use strict',
+        $('#header-carousel .carousel-inner .item').css('height',slideHeight);
+    });
+
+    var wow = new WOW({
+        animateClass: 'animated',
+        offset: 100,
+        mobile: false
+    });
+    wow.init();
+});
